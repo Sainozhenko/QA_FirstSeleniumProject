f671a0d6396b8ed4232d9d8624577bf51c42cad4
Sainozhenko <dmytrosainozhenko@gmail.com>
2023-06-07 21:28:39 +0200

    update

--- src/test/java/com/ait/sainozhenko/tests/FirstSearchElementHw.java
+++ src/test/java/com/ait/sainozhenko/tests/FirstSearchElementHw.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 public class FirstSearchElementHw {
@@ -24,7 +23,13 @@ public class FirstSearchElementHw {
         driver.findElement(By.cssSelector(".master-wrapper-page"));
         driver.findElement(By.cssSelector("#dialog-notifications-error"));
         driver.findElement(By.cssSelector("[title='Close']"));
+        driver.findElement(By.cssSelector("[href='http://www.nopcommerce.com/']"));
+        driver.findElement(By.cssSelector(".footer-store-theme"));
+        driver.findElement(By.cssSelector("#bar-notification"));
+        driver.findElement(By.cssSelector("ul li [href='/search']"));
+        driver.findElement(By.cssSelector("ul li [href='/privacy-policy']"));
     }
+
     @Test
     public void findElementByXpathTest(){
         driver.findElement(By.xpath("//*[@id='dialog-notifications-success']"));
@@ -32,7 +37,10 @@ public class FirstSearchElementHw {
         driver.findElement(By.xpath("//*[@style='display:none;']"));
         driver.findElement(By.xpath("//*[@type='text/javascript']"));
         driver.findElement(By.xpath("//*[contains(.,'Welcome to our store')]"));
-
-
+        driver.findElement(By.xpath("//ul[contains(@class, 'ui-autocomplete')]"));
+        driver.findElement(By.xpath("//ul[contains(@id, 'ui-id-1')]"));
+        driver.findElement(By.xpath("//ul[contains(@tabindex, '0')]"));
+        driver.findElement(By.xpath("//ul[contains(@style, 'display: none;')]"));
+        driver.findElement(By.xpath("//*[@class='footer-store-theme']"));
     }
 }
